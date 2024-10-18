b94c8f953603d1ec773d9eb7ff5db71fbf112fb7
gh-actions <rheannemcintosh@gmail.com>
2024-10-18 23:52:42 +0000

    Laravel Pint

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    public function create(int $number1, int $number2): int {
+    public function create(int $number1, int $number2): int
+    {
         return $number1 + $number2;
     }
 }
